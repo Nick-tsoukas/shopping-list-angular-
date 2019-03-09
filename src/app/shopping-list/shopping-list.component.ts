ac8715209bf4e4bf63b939e63ef7a78c248d08a6
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-09 13:26:44 -0600

    Adding all components needed for shopping list app ...

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.html',
+    styleUrls: ['./shopping-list.css']
+})
+
+export class ShoppingListComponent implements OnInit {
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+  }
+  
+
