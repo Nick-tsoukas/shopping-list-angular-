1e7e376f442231dc379cc96eca211add4ef2b618
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-13 09:57:41 -0500

    Adding recipe detail content and changing nav bar css ...

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe('Steak', 'Steak and Eggs with sea salt and butter', '../../assets/images/steak-compressor.jpg'),
+    new Recipe('Soup', 'Carrot soup made with vegetable and chicken stock', '../../assets/images/soup.jpg'),
+    new Recipe('Steak', 'Steak and Eggs with sea salt and butter', '../../assets/images/steak-compressor.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
