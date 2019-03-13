a0c09e88d2b8872ef06df5d4dd016000751f534a
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-13 10:30:50 -0500

    Adding ingredients model and populating shopping list ...

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+    //  long for of creating model with typescript below
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name:string, amount:number){
+    //     this.name = name;
+    //     this.amount = amount;
+
+    // }
+
+    // shorthand method below 
+    constructor(public name: string, public amount: number){}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ShoppingListComponent implements OnInit {
-
+    ingredients: Ingredient[] = [
+        new Ingredient('apple', 4),
+        new Ingredient('carrort', 3)
+    ];
     constructor() { }
   
     ngOnInit() {
