4f28fbadd260ab9f0f5473782352c99724bbea55
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-03-14 16:03:56 -0500

    Passing data from recipe list comp to recipe item using input decorator ...

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Input() recipes : Recipe[] = [
+   recipes : Recipe[] = [
     new Recipe('Steak', 'Steak and Eggs with sea salt and butter', '../../assets/images/steak-compressor.jpg'),
     new Recipe('Soup', 'Carrot soup made with vegetable and chicken stock', '../../assets/images/soup.jpg'),
     new Recipe('Steak', 'Steak and Eggs with sea salt and butter', '../../assets/images/steak-compressor.jpg')
